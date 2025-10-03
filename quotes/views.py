409aa748603dd6fa6e302c9cfe69c0b6137efbd0
Francis Chung <francis.c.chung@gmail.com>
2025-10-03 15:25:18 +1000

    Code to read API_KEY

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import requests
+import json
+
+from UdemyDjangoStock import settings
 
 # Create your views here.
 def home(request):
+    api_key = settings.API_KEY
+    print("API_KEY", api_key)
+
+    # api_request = requests.get('https://quotes.sina.com')
+
     return render(request, 'home.html', {})
 
 def about(request):
