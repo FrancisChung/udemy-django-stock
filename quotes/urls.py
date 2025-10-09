3698f660a58543f9f0121fb48742433ec95b0514
Francis Chung <francis.c.chung@gmail.com>
2025-10-09 19:37:52 +1100

    Bugfix - Delete URL Pattern

--- quotes/urls.py
+++ quotes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about.html', views.about, name="about"),
     path('home.html', views.home, name="home"),
     path('add_stock.html', views.add_stock, name="add_stock"),
+    path('delete/<int:stock_id>', views.delete_stock, name="delete"),
 ]
